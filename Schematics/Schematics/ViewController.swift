cb6321940bfbd51ec65b84c9c448eda622656fde
Dmytro Trofymenko <dmytro.trofymenko@gmail.com>
2018-04-12 12:05:25 +0300

    Initial project

---
+++ Schematics/Schematics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schematics
+//
+//  Created by Dmytro Trofymenko on 4/11/18.
+//  Copyright © 2018 Quadbase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
